ecc940a945c8d75641a41c69fb9d00267c152545
Yshika Agarwal <yshika.agarwal@geminisolutions.in>
2021-03-15 12:30:48 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResumeFormModule } from './resume-form/resume-form.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'resume',
+    loadChildren: () =>
+      import('./resume-form/resume-form.module').then(
+        (m) => m.ResumeFormModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/resume-form/resume-form-routing.module.ts
+++ src/app/resume-form/resume-form-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateFormComponent } from './create-form/create-form.component';
+import { DeleteFormComponent } from './delete-form/delete-form.component';
+import { UpdateFormComponent } from './update-form/update-form.component';
+import { ViewFormComponent } from './view-form/view-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create-form',
+    component: CreateFormComponent,
+  },
+  {
+    path: 'view-form',
+    component: ViewFormComponent,
+  },
+  {
+    path: 'update-form',
+    component: UpdateFormComponent,
+  },
+  {
+    path: 'delete-form',
+    component: DeleteFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ResumeFormRoutingModule { }
+export class ResumeFormRoutingModule {}
